46f85dc501b634994514e287ab471dc1cef0681e
igor <ispir@reg.ru>
2021-07-12 23:21:54 +0400

    First lesson

---
+++ authors/views.py
@@ -0,0 +1,14 @@
+# from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from .models import Author
+from .serializers import AuthorSerializer
+
+
+class AuthorViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+    # you may override renderer
+    # def get_renderers(self):
